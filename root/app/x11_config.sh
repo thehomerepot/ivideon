b6469d7cf5610bfa361fc82a53627c0b563b317a
thehomerepot <ryan.flagler@gmail.com>
2019-01-30 12:51:59 -0600

    Resolve keyboard issue in X11

--- root/app/x11_config.sh
+++ root/app/x11_config.sh
@@ -4,4 +4,4 @@ echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 usermod abc -s /bin/bash
 chown abc:abc /config/.Xauthority
 rm -f /etc/services.d/videoserver/run
-sudo -i -u abc /opt/ivideon/ivideon-server/ivideon-server
+sudo -i -u abc QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb /opt/ivideon/ivideon-server/ivideon-server
